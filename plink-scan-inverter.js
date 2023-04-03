bb778886cfc528d94300160748ec66d6447d5a4d
hieu-nguyen <nt.hieu.1911@gmail.com>
2023-04-03 15:08:03 +0700

    support im20 and inv 75kw

--- plink-scan-inverter.js
+++ plink-scan-inverter.js
@@ -1,5 +1,6 @@
 const { networkInterfaces } = require('os');
 var modbus = require("modbus-stream");
+const { Client } = require('mqtt');
 
 function ValidateIPaddress(ipaddress) {
     if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipaddress)) {
@@ -28,7 +29,7 @@ const getListIpV4 = () => {
     return results;
 }
 
-const scanInv = (ipDevice, model) => {
+const scanInv = (ipDevice, model, clientId) => {
     return new Promise(function (myResolve, myReject) {
         modbus.tcp.connect(502, ipDevice, { debug: "automaton-2454" }, (err, connection) => {
             // do something with connection
@@ -41,6 +42,7 @@ const scanInv = (ipDevice, model) => {
                     "data": {
                         "device_ip": ipDevice,
                         "serial_number": "",
+                        "clientId": clientId,
                         "err_type": "cannot connect ip"
                     },
                     "error": err
@@ -52,13 +54,14 @@ const scanInv = (ipDevice, model) => {
                     case "SMA-110":
                         //Close connection
                         console.log("case sma 110")
-                        connection.readHoldingRegisters({ address: 40052, quantity: 16, retry: 500, tries: 1, extra: { unitId: 1 } }, (err, res) => {
+                        connection.readHoldingRegisters({ address: 40052, quantity: 16, retry: 500, tries: 1, extra: { unitId: clientId } }, (err, res) => {
                             if (err) {
                                 myResolve({
                                     "success": false,
                                     "data": {
                                         "device_ip": ipDevice,
                                         "serial_number": "",
+                                        "clientId": clientId,
                                         "err_type": "cannot read regs"
                                     },
                                     "error": err
@@ -81,6 +84,7 @@ const scanInv = (ipDevice, model) => {
                                     "data": {
                                         "device_ip": ipDevice,
                                         "serial_number": serialNumber,
+                                        "clientId": clientId,
                                         "err_type": "success read regs, no error"
                                     },
                                     "error": err
@@ -92,13 +96,14 @@ const scanInv = (ipDevice, model) => {
                     case "SMA-100":
                         console.log("case sma 100")
                         //Close connection
-                        connection.readHoldingRegisters({ address: 40052, quantity: 16, retry: 500, tries: 1, extra: { unitId: 126 } }, (err, res) => {
+                        connection.readHoldingRegisters({ address: 40052, quantity: 16, retry: 500, tries: 1, extra: { unitId: clientId } }, (err, res) => {
                             if (err) {
                                 myResolve({
                                     "success": false,
                                     "data": {
                                         "device_ip": ipDevice,
                                         "serial_number": "",
+                                        "clientId": clientId,
                                         "err_type": "cannot read regs"
                                     },
                                     "error": err
@@ -121,6 +126,93 @@ const scanInv = (ipDevice, model) => {
                                     "data": {
                                         "device_ip": ipDevice,
                                         "serial_number": serialNumber,
+                                        "clientId": clientId,
+                                        "err_type": "success read regs, no error"
+                                    },
+                                    "error": err
+                                });
+                            }
+                            connection?.close();
+                        })
+                        break;
+                    case "IM20":
+                        console.log("case im20")
+                        //Close connection
+                        connection.readHoldingRegisters({ address: 40052, quantity: 16, retry: 500, tries: 1, extra: { unitId: clientId } }, (err, res) => {
+                            if (err) {
+                                myResolve({
+                                    "success": false,
+                                    "data": {
+                                        "device_ip": ipDevice,
+                                        "serial_number": "",
+                                        "clientId": clientId,
+                                        "err_type": "cannot read regs"
+                                    },
+                                    "error": err
+                                });
+                            }
+                            else {
+                                dataArr = res.response.data
+                                console.log(dataArr)
+                                serialNumber = ""
+                                dataArr.map((e) => {
+                                    if (e[0] != 0 && e[1] != 0) {
+                                        serialNumber += e.toString()
+                                    }
+                                    else if (e[0] != 0 && e[1] == 0) {
+                                        serialNumber += String.fromCharCode(e[0])
+                                    }
+                                })
+                                console.log(serialNumber);
+                                myResolve({
+                                    "success": true,
+                                    "data": {
+                                        "device_ip": ipDevice,
+                                        "serial_number": serialNumber,
+                                        "clientId": clientId,
+                                        "err_type": "success read regs, no error"
+                                    },
+                                    "error": err
+                                });
+                            }
+                            connection?.close();
+                        })
+                        break;
+                    case "SMA-75":
+                        console.log("case SMA 75")
+                        //Close connection
+                        connection.readHoldingRegisters({ address: 40052, quantity: 16, retry: 500, tries: 1, extra: { unitId: clientId } }, (err, res) => {
+                            if (err) {
+                                myResolve({
+                                    "success": false,
+                                    "data": {
+                                        "device_ip": ipDevice,
+                                        "serial_number": "",
+                                        "clientId": clientId,
+                                        "err_type": "cannot read regs"
+                                    },
+                                    "error": err
+                                });
+                            }
+                            else {
+                                dataArr = res.response.data
+                                console.log(dataArr)
+                                serialNumber = ""
+                                dataArr.map((e) => {
+                                    if (e[0] != 0 && e[1] != 0) {
+                                        serialNumber += e.toString()
+                                    }
+                                    else if (e[0] != 0 && e[1] == 0) {
+                                        serialNumber += String.fromCharCode(e[0])
+                                    }
+                                })
+                                console.log(serialNumber);
+                                myResolve({
+                                    "success": true,
+                                    "data": {
+                                        "device_ip": ipDevice,
+                                        "serial_number": serialNumber,
+                                        "clientId": clientId,
                                         "err_type": "success read regs, no error"
                                     },
                                     "error": err
@@ -155,7 +247,7 @@ module.exports = function (RED) {
         node.on('input', async function (msg) {
             try {
                 const { numberIPPerChunk = 100, model } = config;
-
+                console.log(msg.payload)
                 this.status({ fill: "blue", shape: "ring", text: "scanning" });
                 const listIp = getListIpV4Local();
                 let resultArr = [];
@@ -170,31 +262,66 @@ module.exports = function (RED) {
                     let resultSuccessInNetWork = [];
 
                     resultInNetWork
-                    for (let j = 0; j < 255;) {
-                        let nestedArr = [];
-                        for (let k = 0; k < numberIPPerChunk; k++) {
-                            if (j + k < 255) {
-                                tail = parseInt(k) + parseInt(j)
-                                let reqIp = baseIp + "." + tail;
-                                console.log("reqIp:" + reqIp + ", model: " + model)
-                                // requestArr.push(scanInv(reqIp));
-                                nestedArr.push(scanInv(reqIp, model));
-                            }
-                        };
-                        j = parseInt(j) + parseInt(numberIPPerChunk);
-                        console.log('j: ', j)
-                        const result = await Promise.all(nestedArr);
-                        result.map((res) => {
-                            // console.log(res)
-                            resultInNetWork.push(res);
-                            if (res.success) {
-                                resultSuccessInNetWork.push({
-                                    "device_ip": res?.data?.device_ip,
-                                    "serial_number": res?.data?.serial_number,
-                                });
-                            }
-                        });
+                    if (model == "SMA-75") {
+                        for (let j = 126; j < 169;) {
+                            let nestedArr = [];
+                            for (let k = 0; k < numberIPPerChunk; k++) {
+                                if (j + k < 169) {
+                                    clientId = j + k
+                                    let reqIp = msg.payload
+                                    console.log("reqIp:" + reqIp + ", model: " + model + ", clientId: " + clientId)
+                                    // requestArr.push(scanInv(reqIp));
+                                    nestedArr.push(scanInv(reqIp, model, clientId));
+                                }
+                            };
+                            j = parseInt(j) + parseInt(numberIPPerChunk);
+                            console.log('j: ', j)
+                            const result = await Promise.all(nestedArr);
+                            result.map((res) => {
+                                // console.log(res)
+                                resultInNetWork.push(res);
+                                if (res.success) {
+                                    resultSuccessInNetWork.push({
+                                        "device_ip": res?.data?.device_ip,
+                                        "serial_number": res?.data?.serial_number,
+                                        "clientId": res?.data?.clientId,
+                                    });
+                                }
+                            });
+                        }
                     }
+                    else {
+                        for (let j = 0; j < 255;) {
+                            let nestedArr = [];
+                            for (let k = 0; k < numberIPPerChunk; k++) {
+                                if (j + k < 255) {
+                                    tail = parseInt(k) + parseInt(j)
+                                    let reqIp = baseIp + "." + tail;
+                                    console.log("reqIp:" + reqIp + ", model: " + model)
+                                    // requestArr.push(scanInv(reqIp));
+                                    if (model == "SMA-110") clientId = 1
+                                    else if (model == "SMA-100") clientId = 126
+                                    else if (model == "IM20") clientId = 125
+                                    nestedArr.push(scanInv(reqIp, model, clientId));
+                                }
+                            };
+                            j = parseInt(j) + parseInt(numberIPPerChunk);
+                            console.log('j: ', j)
+                            const result = await Promise.all(nestedArr);
+                            result.map((res) => {
+                                // console.log(res)
+                                resultInNetWork.push(res);
+                                if (res.success) {
+                                    resultSuccessInNetWork.push({
+                                        "device_ip": res?.data?.device_ip,
+                                        "serial_number": res?.data?.serial_number,
+                                        "clientId": res?.data?.clientId,
+                                    });
+                                }
+                            });
+                        }
+                    }
+
                     resultArr.push({
                         "localIp": IPv4,
                         "result": resultSuccessInNetWork
